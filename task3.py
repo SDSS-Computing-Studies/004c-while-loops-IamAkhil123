61ba1c37751d605a5fa2296358f3ed87463669a0
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-15 14:27:16 -0800

    update

--- task3.py
+++ task3.py
@@ -1,26 +1,20 @@
 #! python3
 
-from asyncio import staggered
-from distutils.command.sdist import sdist
-from readline import set_completion_display_matches_hook
+while True:
+    number = float(input("enter a number"))
+    x = int(number)
+    if x == 0:
+        break
 
+    y = x % 2
 
-x = input("enter a number")
-print("That is not an even integer")
-x = input("enter a number")
-print("That is an even integer")
-if x % 2 == 0:
-  print("That is an even integer")
-else:
-  x = int(input("enter a number"))
-  x = input("That is not an even integer")
-  
-  
-print("Enter a number")
+    if y > 0:
+        print("That is not an even integer")
+    else:
+        print("That is an even integer")
+        exit()
 
-input()
-
-"""
+"""  
 Ask the user to enter in a number.
 Have them repeat this until the number is an even integer.
 (2 marks)
@@ -36,7 +30,6 @@ Enter number:1.3
 That is not an even integer
 Enter number:4
 That is an even integer
-
 """
 
 
